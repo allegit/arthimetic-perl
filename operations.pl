594ca6b138c177f538b2f07563824ca5833e96aa
allegit <vasanth.alle@gmail.com>
2019-07-17 08:33:16 -0400

    Comment
    Added another comment: year

--- operations.pl
+++ operations.pl
@@ -1,5 +1,6 @@
 #!/usr/bin/perl -w
 # Author: Vasanth Alle
+# 2018
 
 my $var1 = 2;
 my $var2 = 3;
@@ -10,7 +11,7 @@ my $oper = <>;
 #my $oper = "+";
 my $result;
 if ($oper eq "+") {
-	$result = add($var1,$var2);	
+	$result = add($var1,$var2);
 } else {
 	$result = subtract($var1,$var2);
 }
@@ -20,7 +21,7 @@ print "Result of adding $var1 and $var2 is $result\n";
 sub add
 {
 	my ($v1,$v2) = @_;
-	
+
 	my $result_add = $v1 + $v2;
 	return $result_add;
 }
